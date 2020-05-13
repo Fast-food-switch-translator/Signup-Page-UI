8b1e7d1cdff86524f5616623affa18662ea42c6e
Aalyousif <aalyousif@deakin.edu.au>
2020-05-13 22:10:47 +1000

    Signup-ui

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+//import 'pages/home_page.dart';
+import 'pages/register_page.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(MyApp());
+
+  SystemChrome.setSystemUIOverlayStyle(lightSystemUiOverlayStyle);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Birb',
+      theme: buildThemeData(),
+      home: const RegisterPage(),
+      routes: <String, WidgetBuilder>{
+        RegisterPage.routeName: (BuildContext context) => const RegisterPage(),
+      },
+    );
+  }
+}
